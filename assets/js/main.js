dac974c8c9bfa030ca75f4ad16ed6f93c637e6fb
kamal khatri <kamal1997khatri@gmail.com>
2022-09-11 12:07:37 +0545

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,36 @@ $(window).scroll(function () {
 });
 
 AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // autoplay: {
+  //   delay: 5000,
+  //   disableOnInteraction: true,
+  // },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  keyboard: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1199: {
+      slidesPerView: 4,
+      spaceBetween: 25,
+    },
+  },
+});
